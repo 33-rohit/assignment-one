14d0b3bad174aeb37e73eb982d45f7aaac1857ba
Rohit <tailorrohit06@gmail.com>
2021-08-18 13:55:08 +0530

    Update 1

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    let name = "Rohit tailor";
+    return(
+        <div>
+        <h1>Header Component</h1>
+        <h2>Myself {name} </h2>
+       
+        <h3>this is 1st Component</h3>
+      
+       </div>
+    )
+}
+export default Header;
